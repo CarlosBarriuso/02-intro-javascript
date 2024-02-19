c31c99f8b21ef01903af614aeaa7922149cbc15b
carlos <carlosbarriuso@yahoo.es>
2024-02-19 17:14:10 +0100

    resolve de la promesa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 //Promesas
 
-const promesa = new Promise();
+const promesa = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        resolve();
+    }, 2000 )
+});
+ 
+promesa.then( () => {
+    console.log('Then de la promesa');
+})
